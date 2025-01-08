036ffa01bf5192e42a6f599243242254f2d964fd
anushkaweavers <anushka.goswami@weavers-web.com>
2025-01-08 16:41:48 +0530

    created database collections

--- src/models/posts.model.js
+++ src/models/posts.model.js
@@ -8,9 +8,17 @@ const postSchema = new mongoose.Schema(
     media: { type: [String] },
     tags: { type: [String] },
     rating: { type: Number, min: 1, max: 10 },
+    generalScore: { type: Number, min: 0, max: 10 },
+    plotScore: { type: Number, min: 0, max: 10 },
+    storyScore: { type: Number, min: 0, max: 10 },
+    characterScore: { type: Number, min: 0, max: 10 },
+    cinematographyScore: { type: Number, min: 0, max: 10 },
+    rateScore: { type: Number, min: 0, max: 10 },
     comments: [{ userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }, comment: String }],
     likes: { type: [mongoose.Schema.Types.ObjectId], ref: 'User', default: [] },
     shares: { type: [mongoose.Schema.Types.ObjectId], ref: 'User', default: [] },
+    filters: { type: Object },
+    mediaType: { type: String },
   },
   { timestamps: true }
 );
