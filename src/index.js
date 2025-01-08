25de9583d386f5127bf5a5560c46571fc7ad7eae
anushkaweavers <anushka.goswami@weavers-web.com>
2025-01-08 13:16:58 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const config = require('./config/config');
 const logger = require('./config/logger');
 
 let server;
-mongoose.connect(config.mongoose.url, config.mongoose.options).then(() => {
+mongoose.connect('mongodb://localhost:27017/movie_share', config.mongoose.options).then(() => {
   logger.info('Connected to MongoDB');
   server = app.listen(config.port, () => {
     logger.info(`Listening to port ${config.port}`);
