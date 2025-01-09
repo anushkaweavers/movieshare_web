7f6bdeb3abf3925bccde3123843acefc524d1bbc
anushkaweavers <anushka.goswami@weavers-web.com>
2025-01-09 15:48:45 +0530

    user registration configured and tetsed

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -4,7 +4,7 @@ const UserModel = require('../models/user.model');
 exports.registerUser = async (userData) => {
   const { firstName, lastName, username, email, passwordHash, termsAccepted } = userData;
 
-  // Check if the email or username already exists
+
   const existingUser = await UserModel.findOne({
     $or: [{ email }, { username }],
   });
@@ -12,10 +12,8 @@ exports.registerUser = async (userData) => {
     throw new Error('Email or username is already in use');
   }
 
-  // Hash the password
   const hashedPassword = await bcrypt.hash(passwordHash, 10);
 
-  // Create a new user
   const newUser = await UserModel.create({
     firstName,
     lastName,
