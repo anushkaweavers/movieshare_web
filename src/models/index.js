388f99fb5ee37eb5f3aba733d59340e18cce6159
anushkaweavers <anushka.goswami@weavers-web.com>
2025-01-13 13:49:02 +0530

    updated frontend strcuture

--- src/models/index.js
+++
@@ -1,8 +0,0 @@
-module.exports.Token = require('./token.model');
-module.exports.User = require('./user.model');
-module.exports.Admin = require('./admin.models');
-module.exports.GroupedCollections = require('./groupedCollections.model');
-module.exports.Posts = require('./posts.model');
-module.exports.Ratings = require('./ratings.model');
-module.exports.SavedMovies = require('./savedMovies.model');
-module.exports.TemporarySearchResults = require('./temporary.SearchResults.model');
