ca40036a16b5f1e43a4ed85d482a0eec53a5280a
anushkaweavers <anushka.goswami@weavers-web.com>
2025-01-17 13:05:04 +0530

    adjusted forgot-pw page

--- client/src/components/Auth/Forgot-Password/index.jsx
+++ client/src/components/Auth/Forgot-Password/index.jsx
@@ -1,10 +1,14 @@
 import React, { useState } from "react";
-import { Box, Container, FormGroup, Grid, Dialog, DialogContent, Button } from "@mui/material";
+import {
+  Box,
+  Container,
+  FormGroup,
+  Grid,
+  Dialog,
+  DialogContent,
+  Button,
+} from "@mui/material";
 import { Link } from "react-router-dom";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation } from "swiper/modules";
-import "swiper/css";
-import "swiper/css/navigation";
 import TextFieldInput from "../Common/UiComps/TextField";
 import ButtonField from "../Common/UiComps/ButtonField";
 import FullScreenLoader from "../Common/UiComps/FullScreenLoader";
@@ -22,91 +26,48 @@ const ForgotPassword = () => {
   const [open, setOpen] = useState(false);
 
   return (
-    <Container maxWidth={false} className="auth-wrapper">
+    <Container
+      maxWidth={false}
+      className="auth-wrapper"
+      style={{ minHeight: "100vh", display: "flex", alignItems: "stretch" }}
+    >
       <Grid container spacing={0} className="auth-wrapper-inn">
-        <Grid item md={7} sm={12} xs={12}>
-          <Box className="auth-slider-wrap">
-            <Swiper
-              slidesPerView={1}
-              loop
-              modules={[Navigation]}
-              className="authSwiper"
-              navigation
-            >
-              <SwiperSlide>
-                <Box className="auth-slider-img-holder">
-                  <img
-                    src="/images/signup-slider-img1.jpg"
-                    alt="Slider 1"
-                    className="slider-img"
-                  />
-                  <Box className="auth-slider-info">
-                    <Box className="auth-slider-info-inner">
-                      <h1>
-                        Start Your <br /> Journey With Us.
-                      </h1>
-                      <p>
-                        Lorem ipsum dolor sit amet consectetur. Integer vel sed enim aliquet volutpat adipiscing ante amet.
-                      </p>
-                    </Box>
-                  </Box>
-                </Box>
-              </SwiperSlide>
-              <SwiperSlide>
-                <Box className="auth-slider-img-holder">
-                  <img
-                    src="/images/signup-slider-img1.jpg"
-                    alt="Slider 2"
-                    className="slider-img"
-                  />
-                  <Box className="auth-slider-info">
-                    <Box className="auth-slider-info-inner">
-                      <h1>
-                        Start Your <br /> Journey With Us.
-                      </h1>
-                      <p>
-                        Lorem ipsum dolor sit amet consectetur. Integer vel sed enim aliquet volutpat adipiscing ante amet.
-                      </p>
-                    </Box>
-                  </Box>
-                </Box>
-              </SwiperSlide>
-              <SwiperSlide>
-                <Box className="auth-slider-img-holder">
-                  <img
-                    src="/images/signup-slider-img1.jpg"
-                    alt="Slider 3"
-                    className="slider-img"
-                  />
-                  <Box className="auth-slider-info">
-                    <Box className="auth-slider-info-inner">
-                      <h1>
-                        Start Your <br /> Journey With Us.
-                      </h1>
-                      <p>
-                        Lorem ipsum dolor sit amet consectetur. Integer vel sed enim aliquet volutpat adipiscing ante amet.
-                      </p>
-                    </Box>
-                  </Box>
-                </Box>
-              </SwiperSlide>
-            </Swiper>
-          </Box>
+        {/* Left Section */}
+        <Grid
+          item
+          md={7}
+          sm={12}
+          xs={12}
+          style={{
+            height: "100%",
+            display: "flex",
+            flexDirection: "column",
+          }}
+        >
+          <LeftSection />
         </Grid>
 
+        {/* Right Section */}
         <Grid
           item
           md={5}
           sm={12}
           xs={12}
           className="auth-form-outer signup-form-wrap"
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            height: "100%",
+          }}
         >
           <Box className="auth-form-wrap">
             <Box className="auth-form-top">
               <Link to="/login">Back to Login</Link>
               <h2>Reset Password</h2>
               <p>
-                Enter the email associated with your account and we’ll send an email with instructions to reset your password.
+                Enter the email associated with your account and we’ll send an
+                email with instructions to reset your password.
               </p>
             </Box>
 
@@ -143,6 +104,8 @@ const ForgotPassword = () => {
           </Box>
         </Grid>
       </Grid>
+
+      {/* Loader */}
       <FullScreenLoader open={isPending} />
 
       {/* Confirmation Dialog */}
@@ -173,4 +136,3 @@ const ForgotPassword = () => {
 };
 
 export default ForgotPassword;
-
